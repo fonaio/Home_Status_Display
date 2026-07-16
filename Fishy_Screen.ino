5ebf4e24b92da3dc274f9fabe5643e10fb240423
fonaio <flin3004825@gmail.com>
2026-07-16 14:39:58 -0500

    set up fish swimming variables

--- Fishy_Screen.ino
+++ Fishy_Screen.ino
@@ -3,7 +3,7 @@
 #include <Adafruit_GFX.h>
 #include <SPI.h>
 #include <WiFiClientSecure.h>
-#include <PubSubClient.h> 
+#include <PubSubClient.h>
 
 #include "Coral_Reef.c"
 #include "Nemo_1.c"
@@ -20,7 +20,7 @@
 
 //MQTT information
 const char* MQTT_HOST = "23c7c9e727f2450999e63ac8d5f5eda0.s1.eu.hivemq.cloud";
-const int   MQTT_PORT = 8883;
+const int MQTT_PORT = 8883;
 const char* MQTT_USER = "FishFish";
 const char* MQTT_PASS = "BlubBlubBlub00";
 
@@ -41,7 +41,7 @@ Adafruit_GC9A01A tft(TFT_CS, TFT_DC, TFT_COPI, TFT_SCLK, TFT_RST, -1);
 #define MOTION 20  //D7, GPIO20
 
 //home, away
-#define DND_BUTTON 7     //D5, GPIO7
+#define DND_BUTTON 7       //D5, GPIO7
 #define PRESENCE_BUTTON 6  //D4, GPIO6
 
 //old definitions for printing out status
@@ -51,7 +51,7 @@ Adafruit_GC9A01A tft(TFT_CS, TFT_DC, TFT_COPI, TFT_SCLK, TFT_RST, -1);
 #define TEXT_H 20*/
 
 //fish animation (home + DND)
-#define MAGENTA 0xF81F //make this magenta color act as a transparent block
+#define MAGENTA 0xF81F  //make this magenta color act as a transparent block
 typedef const uint8_t (*FishAnimation)[800];
 
 enum BiomeType {
@@ -64,7 +64,7 @@ BiomeType biome = CORAL_REEF;
 
 FishAnimation coralReefFish[3] = {
   jellyfish1_map,
-  Pink_1_map, 
+  Pink_1_map,
   nemo_1_map
 };
 
@@ -95,7 +95,7 @@ const uint16_t* Nemo_map[5] = {
 
 FishAnimation deepSeaFish[3] = {
   angler1_map,
-  squid1_map, 
+  squid1_map,
   Grey_1_map
 };
 
@@ -132,86 +132,84 @@ const uint16_t* musicNotes_map[2] = {
 const uint16_t* getBiomeMap(BiomeType biome) {
   //background lookup
   switch (biome) {
-    case CORAL_REEF: return (const uint16_t*) Coral_Reef_map;
-    case DEEP_SEA:   return (const uint16_t*)Deep_Sea_map;
-    case AWAY_BIOME:       return (const uint16_t*)Away1_map;
+    case CORAL_REEF: return (const uint16_t*)Coral_Reef_map;
+    case DEEP_SEA: return (const uint16_t*)Deep_Sea_map;
+    case AWAY_BIOME: return (const uint16_t*)Away1_map;
   }
-  return (const uint16_t*) Coral_Reef_map;
+  return (const uint16_t*)Coral_Reef_map;
 }
 
-FishAnimation chooseRandomFish(BiomeType biome){
+FishAnimation chooseRandomFish(BiomeType biome) {
   //Chooses a random fish depending on the 'biome'
   int randomIndex = random(0, 3);
 
-  if (biome == CORAL_REEF){
+  if (biome == CORAL_REEF) {
     return coralReefFish[randomIndex];
   }
 
-  if(biome == DEEP_SEA){
+  if (biome == DEEP_SEA) {
     return deepSeaFish[randomIndex];
   }
 }
 
-void patchUpdate(const uint16_t* fullMap, int x0, int y0, int w, int h){
+void patchUpdate(const uint16_t* fullMap, int x0, int y0, int w, int h) {
   //creates a background patch corresponding to 240x240 map
-  uint16_t patch[w*h];
-  for (int row = 0; row < h; row++){
+  uint16_t patch[w * h];
+  for (int row = 0; row < h; row++) {
     const uint16_t* srcRow = fullMap + (y0 + row) * 240 + x0;
-    memcpy(&patch[row * w], srcRow, w*sizeof(uint16_t));
+    memcpy(&patch[row * w], srcRow, w * sizeof(uint16_t));
   }
   tft.drawRGBBitmap(x0, y0, patch, w, h);
 }
 
-void drawSprite(const uint16_t* spriteFrame, int x0, int y0, int w, int h){
+void drawSprite(const uint16_t* spriteFrame, int x0, int y0, int w, int h) {
   //draws the sprite (fish or music), ignoring magenta pixels (0x1FF8)
   uint16_t rowBuf[w];
-  for (int row = 0; row < h; row++){
+  for (int row = 0; row < h; row++) {
     int col = 0;
-    while(col < w){
-      if(spriteFrame[row * w + col] == MAGENTA) { //ignore magenta blocks
+    while (col < w) {
+      if (spriteFrame[row * w + col] == MAGENTA) {  //ignore magenta blocks
         col++;
         continue;
-    }
-    int runstart = col;
-    int runLen = 0;
-    while (col < w && spriteFrame[row * w + col] != MAGENTA) {
+      }
+      int runstart = col;
+      int runLen = 0;
+      while (col < w && spriteFrame[row * w + col] != MAGENTA) {
         rowBuf[runLen] = spriteFrame[row * w + col];
         runLen++;
         col++;
-    }
-    tft.drawRGBBitmap(x0 + runstart, y0 + row, rowBuf, runLen, 1);
+      }
+      tft.drawRGBBitmap(x0 + runstart, y0 + row, rowBuf, runLen, 1);
     }
   }
 }
 
-void animateSwim(FishAnimation fishToDraw){
-  int screenWidth = 240; 
+void animateSwim(FishAnimation fishToDraw) {
+  int screenWidth = 240;
   int screenHeight = 240;
-  int fishWidth = 20;  
+  int fishWidth = 20;
   int fishHeight = 20;
 
   int currentY = random(20, screenHeight - fishHeight);
 
-  int currentX = 0;
-  int distanceX = 10; //distance between each frame
+  int currentX = -fishWidth;
+  int speedX = random(1, 5);  //distance between each frame
+
   int frameindex = 0;
 
-  while (currentX < screenWidth){
+  while (currentX < screenWidth) {
     /* Draw fish, wipe, update next spawn location, draw next frame*/
-
   }
 }
 
-void showHome(){ // only show coral reef fish
+void showHome() {  // only show coral reef fish
   tft.drawRGBBitmap(0, 0, (const uint16_t*)Coral_Reef_map, 240, 240);
 }
-void showDND(){ //deep sea fish
+void showDND() {  //deep sea fish
   tft.drawRGBBitmap(0, 0, (const uint16_t*)Deep_Sea_map, 240, 240);
 }
-
-void showAway(){ //fisherman
+void showAway() {  //fisherman
   tft.drawRGBBitmap(0, 0, (const uint16_t*)Away1_map, 240, 240);
-
 }
 
 //timing
@@ -225,13 +223,13 @@ const int PATCH_Y = 84;
 const int PATCH_W = 18;
 const int PATCH_H = 25;
 
-void animateMusic(){
-//animates the music notes. Draws a frame, patches, draws another frame on top
+void animateMusic() {
+  //animates the music notes. Draws a frame, patches, draws another frame on top
   int currTime = millis();
-  if (currTime - lastFrameTime > frameInterval){
+  if (currTime - lastFrameTime > frameInterval) {
     lastFrameTime = currTime;
     patchUpdate(getBiomeMap(biome), PATCH_X, PATCH_Y, PATCH_W, PATCH_H);
-    currFrame = (1+currFrame)%2;
+    currFrame = (1 + currFrame) % 2;
     drawSprite(musicNotes_map[currFrame], PATCH_X, PATCH_Y, PATCH_W, PATCH_H);
   }
 }
@@ -274,8 +272,8 @@ void loop() {
   bool presencePressed = (presenceRead && !lastPresence);
   bool dndPressed = (dndRead && !lastDnd);
 
-//we only want to run this code when the button goes from low to high
-  if (presencePressed || dndPressed) { 
+  //we only want to run this code when the button goes from low to high
+  if (presencePressed || dndPressed) {
     if (presencePressed) {
       presence_state = !presence_state;
       Serial.println("Home/Away button pressed");
@@ -284,28 +282,30 @@ void loop() {
       dnd_state = !dnd_state;
       Serial.println("DND button pressed");
     }
-    
+
     if (presence_state && !dnd_state) {
       showHome();
       currentStatus = HOME;
     }
-    
+
     else if (presence_state && dnd_state) {
       showDND();
       currentStatus = DND;
     }
-    
-    else { //Away + DND or Away + !DND
+
+    else {  //Away + DND or Away + !DND
       showAway();
       currentStatus = AWAY;
     }
   }
   if (currentStatus == HOME) {
     biome = CORAL_REEF;
+    //animateSwim(chooseRandomFish(biome));
   }
-  
+
   if (currentStatus == DND) {
     biome = DEEP_SEA;
+    //animateSwim(chooseRandomFish(biome));
   }
 
   if (currentStatus == AWAY) {
